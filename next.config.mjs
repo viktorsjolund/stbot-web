d1498544d984f7c38c225695c2b472ff0dce6aaa
vs222qj <vs222qj@student.lnu.se>
2024-01-30 17:27:46 +0100

    add twitch image host to config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [{
+      hostname: 'static-cdn.jtvnw.net',
+      protocol: 'https',
+      pathname: '/jtv_user_pictures/**'
+    }]
+  }
+};
 
 export default nextConfig;
