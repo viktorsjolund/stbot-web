f466a3294b2470a4145ef566fb6ce9eb8b836399
vs222qj <vs222qj@student.lnu.se>
2024-02-01 15:19:30 +0100

    add get active users to api

---
+++ src/app/api/active/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { prisma } from '@/prisma'
+import { ActiveUser, User } from '@prisma/client'
+
+export async function GET(req: NextRequest) {
+  let activeUsers: (ActiveUser & { user: User })[]
+
+  try {
+    activeUsers = await prisma.activeUser.findMany({
+      include: {
+        user: true
+      }
+    })
+  } catch (e) {
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
+  }
+
+  const usernames = activeUsers.map((u) => u.user.name)
+
+  return NextResponse.json({ users: usernames })
+}
