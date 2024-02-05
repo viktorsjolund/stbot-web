05bba26edeb622af6f1edb6d7859bac7dab8f0e3
vs222qj <vs222qj@student.lnu.se>
2024-02-05 16:10:41 +0100

    add message broker responses

--- src/app/api/webhook/song-redeem/route.ts
+++ src/app/api/webhook/song-redeem/route.ts
@@ -2,12 +2,12 @@ import axios from 'axios'
 import { getSpotifyAccessToken } from '@/util/spotify'
 import { HmacHandler } from '@/util/hmacHandler'
 import { NextRequest, NextResponse } from 'next/server'
-import { NextApiResponse } from 'next'
 import { prisma } from '@/prisma'
 import { User } from '@prisma/client'
+import { sendMessageToQueue } from '@/util/messageBroker'
 const messageIds = new Set<string>()
 
-async function handler(req: NextRequest, res: NextApiResponse) {
+async function handler(req: NextRequest) {
   const hmh = new HmacHandler()
   const rawBody = Buffer.from(await req.arrayBuffer())
   const body = JSON.parse(rawBody.toString())
@@ -34,7 +34,7 @@ async function handler(req: NextRequest, res: NextApiResponse) {
   const messageId = req.headers.get('twitch-eventsub-message-id')![0]
 
   if (messageIds.has(messageId)) {
-    return NextResponse.json('OK', { status: 200 })
+    return NextResponse.json({ message: 'OK' }, { status: 200 })
   } else {
     messageIds.add(messageId)
     setTimeout(() => messageIds.delete(messageId), 1000 * 60 * 5)
@@ -66,7 +66,7 @@ async function handler(req: NextRequest, res: NextApiResponse) {
 
   const accessToken = await getSpotifyAccessToken(user.spotifyRefreshToken)
   if (!accessToken) {
-    return NextResponse.json('OK', { status: 200 })
+    return NextResponse.json({ error: 'Could not get access token.' }, { status: 500 })
   }
   let trackId = ''
 
@@ -77,7 +77,7 @@ async function handler(req: NextRequest, res: NextApiResponse) {
     const tokens = input.split('-')
 
     if (tokens.length !== 2) {
-      return NextResponse.json('OK', { status: 200 })
+      return NextResponse.json({ error: 'Bad input' }, { status: 400 })
     }
 
     const artist = tokens[0].trim()
@@ -99,8 +99,22 @@ async function handler(req: NextRequest, res: NextApiResponse) {
 
       trackId = result.data.tracks.items[0].uri.split(':')[2]
     } catch (e) {
-      return NextResponse.json('OK', { status: 200 })
+      return NextResponse.json({ error: 'Something went wrong...' }, { status: 500 })
     }
+  }
+
+  let track
+  try {
+    const result = await axios(`https://api.spotify.com/v1/tracks/${trackId}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    })
+    track = result.data
+  } catch (e) {
+    await sendMessageToQueue(user.name!, 'Could not find song.')
+    return NextResponse.json({ error: `Could not find song with id: ${trackId}` }, { status: 400 })
   }
 
   try {
@@ -111,11 +125,13 @@ async function handler(req: NextRequest, res: NextApiResponse) {
       },
       params: { uri: `spotify:track:${trackId}` }
     })
+    await sendMessageToQueue(user.name!, `> ${track.artists[0].name} - ${track.name}`)
   } catch (e) {
-    return NextResponse.json('OK', { status: 200 })
+    await sendMessageToQueue(user.name!, 'Could not queue song.')
+    return NextResponse.json({ error: 'Could not queue song' }, { status: 400 })
   }
 
-  return NextResponse.json('OK', { status: 200 })
+  return NextResponse.json({ message: 'OK' }, { status: 200 })
 }
 
 export { handler as GET, handler as POST }
---
+++ src/util/messageBroker.ts
@@ -0,0 +1,43 @@
+import amqp from 'amqplib'
+
+export async function sendMessageToQueue(channel: string, message: string) {
+  try {
+    const msg = `PRIVMSG #${channel} :${message}`
+    await sendToQueue(msg)
+  } catch (e) {
+    throw e
+  }
+}
+
+export async function sendPartToQueue(channel: string) {
+  try {
+    const msg = `PART #${channel}`
+    await sendToQueue(msg)
+  } catch (e) {
+    throw e
+  }
+}
+export async function sendJoinToQueue(channel: string) {
+  try {
+    const msg = `JOIN #${channel}`
+    await sendToQueue(msg)
+  } catch (e) {
+    throw e
+  }
+}
+
+async function sendToQueue(message: string) {
+  try {
+    const conn = await amqp.connect(process.env.AMQP_URL!)
+    const ch = await conn.createChannel()
+    const queue = 'send'
+    await ch.assertQueue(queue, { durable: false })
+    ch.sendToQueue(queue, Buffer.from(message))
+    console.log(`[x] Sent ${message}`)
+    await ch.close()
+    await conn.close()
+  } catch (e) {
+    throw e
+  }
+
+}
