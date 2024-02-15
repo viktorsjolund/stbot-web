1740fc2ae2e31ab1914304d68f1982d2968abb84
vs222qj <vs222qj@student.lnu.se>
2024-02-15 16:55:02 +0100

    changed hmac handler to follow FP

--- src/util/hmacHandler.ts
+++ src/util/hmacHandler.ts
@@ -1,30 +1,28 @@
 import crypto from 'crypto'
 import { NextRequest } from 'next/server'
 
-export class HmacHandler {
-  getHmacMessage(req: NextRequest, body: Buffer) {
-    if (
-      !req.headers.get('twitch-eventsub-message-id') ||
-      !req.headers.get('twitch-eventsub-message-timestamp')
-    ) {
-      return ''
-    }
-
-    return (
-      req.headers.get('twitch-eventsub-message-id')! +
-      req.headers.get('twitch-eventsub-message-timestamp')! +
-      body
-    )
+export function getHmacMessage(req: NextRequest, body: Buffer) {
+  if (
+    !req.headers.get('twitch-eventsub-message-id') ||
+    !req.headers.get('twitch-eventsub-message-timestamp')
+  ) {
+    return ''
   }
 
-  getHmac(message: string) {
-    return crypto
-      .createHmac('sha256', process.env.TWITCH_WEBHOOK_CALLBACK_SECRET!)
-      .update(message)
-      .digest('hex')
-  }
+  return (
+    req.headers.get('twitch-eventsub-message-id')! +
+    req.headers.get('twitch-eventsub-message-timestamp')! +
+    body
+  )
+}
 
-  verifyMessage(hmac: string, verifySignature: string) {
-    return crypto.timingSafeEqual(Buffer.from(hmac), Buffer.from(verifySignature))
-  }
+export function getHmac(message: string) {
+  return crypto
+    .createHmac('sha256', process.env.TWITCH_WEBHOOK_CALLBACK_SECRET!)
+    .update(message)
+    .digest('hex')
+}
+
+export function verifyMessage(hmac: string, verifySignature: string) {
+  return crypto.timingSafeEqual(Buffer.from(hmac), Buffer.from(verifySignature))
 }
