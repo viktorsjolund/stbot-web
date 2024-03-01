053ec813fda01ce81b2e22cfd5826694e1bebb82
vs222qj <vs222qj@student.lnu.se>
2024-03-01 15:47:18 +0100

    add about page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,15 +9,20 @@ export const Header = async () => {
 
   return (
     <div className='h-14 bg-slate-900 flex items-center fixed w-full shadow-md'>
-      <Link href='/'>
-        <div className='ml-6 cursor-pointer pt-1 pb-1 pr-2 pl-2 rounded hover:bg-slate-800 transition-colors'>
-          <span className='tracking-widest text-xl font-extrabold'>
-            <span className='text-green-600'>S</span>
-            <span className='text-purple-600'>T</span>
-            <span>BOT</span>
-          </span>
-        </div>
-      </Link>
+      <div className='flex items-center space-x-4'>
+        <Link href='/'>
+          <div className='ml-6 cursor-pointer pt-1 pb-1 pr-2 pl-2 rounded hover:bg-slate-800 transition-colors'>
+            <span className='tracking-widest text-xl font-extrabold'>
+              <span className='text-green-600'>S</span>
+              <span className='text-purple-600'>T</span>
+              <span>BOT</span>
+            </span>
+          </div>
+        </Link>
+        <Link href='/about'>
+          <span className='font-bold tracking-wider text-slate-300 hover:text-slate-200 transition-colors'>ABOUT</span>
+        </Link>
+      </div>
       <div className='ml-auto pr-12'>
         {session?.user ? <LoggedIn user={session.user} /> : <LoggedOut />}
       </div>
