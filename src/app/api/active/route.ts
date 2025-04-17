3c4151892062fe6d40d245e86370f2cfb9700efd
vs222qj <vs222qj@student.lnu.se>
2025-04-17 20:34:53 +0200

    changed active users to only return streams which are currently live

--- src/app/api/active/route.ts
+++ src/app/api/active/route.ts
@@ -1,23 +1,58 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/prisma'
-import { ActiveUser, User } from '@prisma/client'
+import { Account, ActiveUser, User } from '@prisma/client'
+import { getTwitchAppAccessToken } from '@/util/twitch'
+import axios from 'axios'
 
 export async function GET(req: NextRequest) {
-  let activeUsers: (ActiveUser & { user: User })[]
+  let activeUsers: (ActiveUser & { user: User & { accounts: Account[] } })[]
 
   try {
     activeUsers = await prisma.activeUser.findMany({
       include: {
-        user: true
-      }
+        user: {
+          include: {
+            accounts: true,
+          },
+        },
+      },
     })
   } catch (e) {
-    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 },
+    )
   }
 
-  const usernames = activeUsers.map((u) => u.user.name)
+  const accessToken = await getTwitchAppAccessToken()
+  const broadcasterIds = activeUsers.map(
+    (u) =>
+      u.user.accounts.filter((a) => a.provider === 'twitch')[0]
+        .providerAccountId,
+  )
 
-  return NextResponse.json({ users: usernames })
+  if (broadcasterIds.length === 0) {
+    return NextResponse.json({ users: [] })
+  }
+
+  try {
+    const liveStreams = await axios.get('https://api.twitch.tv/helix/streams', {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Client-Id': process.env.TWITCH_CLIENT_ID,
+      },
+      params: {
+        user_id: broadcasterIds,
+        type: 'live',
+      },
+    })
+    return NextResponse.json({ users: liveStreams.data.data })
+  } catch (e) {
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 },
+    )
+  }
 }
 
 export const dynamic = 'force-dynamic'
