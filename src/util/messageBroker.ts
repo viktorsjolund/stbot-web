a9f6853bfd65a59689116427adf0d4ec6b300a45
vs222qj <vs222qj@student.lnu.se>
2026-02-25 02:58:09 +0100

    modified amqp connection to be singleton and changed frameMax and heartbeat values

--- src/util/messageBroker.ts
+++ src/util/messageBroker.ts
@@ -1,4 +1,5 @@
 import amqp from 'amqplib'
+let connection: amqp.Connection
 
 export async function sendMessageToQueue(channel: string, message: string) {
   try {
@@ -27,15 +28,34 @@ export async function sendJoinToQueue(channel: string) {
 }
 
 async function sendToQueue(message: string) {
+  if (!connection) {
+    try {
+      if (process.env.NODE_ENV === 'development') {
+        connection = await amqp.connect(process.env.AMQP_URL!)
+      } else {
+        connection = await amqp.connect({
+          heartbeat: 60,
+          frameMax: 0,
+          password: process.env.AMQP_PASSWORD,
+          username: process.env.AMQP_USERNAME,
+          hostname: process.env.AMQP_HOSTNAME,
+          port: 5671,
+          vhost: process.env.AMQP_USERNAME,
+          protocol: 'amqps',
+        })
+      }
+    } catch (e) {
+      throw e
+    }
+  }
+
   try {
-    const conn = await amqp.connect(process.env.AMQP_URL!)
-    const ch = await conn.createChannel()
+    const ch = await connection.createChannel()
     const queue = 'send'
     await ch.assertQueue(queue, { durable: false })
     ch.sendToQueue(queue, Buffer.from(message))
     console.log(`[x] Sent ${message}`)
     await ch.close()
-    await conn.close()
   } catch (e) {
     throw e
   }
